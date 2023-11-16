a47b6a71c57b5d2eec689efbe23c135fbf6fb045
Hubert Haładyna <hubert.haladyna@microsoft.wsei.edu.pl>
2023-11-16 08:32:43 +0100

    Add project files.

---
+++ Wzorki/kfadrat.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Channels;
+
+class kfadrat
+{
+    const char CHAR = '*';
+    static void Star() => Console.Write(CHAR);
+    static void StarLn() => Console.WriteLine(CHAR);
+    static void Space() => Console.Write(" ");
+    static void SpaceLn() => Console.WriteLine(" ");
+    static void NewLine() => Console.WriteLine();
+
+    public static void Prostokat(int n, int m)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            Star();
+        }
+        NewLine();
+
+        for (int j = 1; j < m - 1; j++)
+        {
+            Star();
+            for (int i = 1; i < n - 1; i++)
+            {
+                Space();
+            }
+            StarLn();
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            Star();
+        }
+        NewLine();
+    }
+
+    public static void Main()
+    {
+        Prostokat(10, 7);
+    }
+}
---
+++ literax/iks.cs
@@ -0,0 +1,61 @@
+﻿using System.Threading.Channels;
+
+class iks
+{
+    const char CHAR = '*';
+    static void Star() => Console.Write(CHAR);
+    static void StarLn() => Console.WriteLine(CHAR);
+    static void Space() => Console.Write(" ");
+    static void SpaceLn() => Console.WriteLine(" ");
+    static void NewLine() => Console.WriteLine();
+
+    static void LiteraX(int n)
+    {
+        if (n < 3) throw new ArgumentException("zbyt mały rozmiar");
+        if (n % 2 == 0) n = n + 1;
+
+        //górna połówka
+        for (int i = 0; i < n / 2; i++)
+        {
+            for (int j = 0; j < i; j++) //spacje przed
+            {
+                Space();
+            }
+            Star();
+            for (int j = 0; j < n - 2 - 2 * i; j++) //spacje w środku
+            {
+                Space();
+            }   
+            StarLn();
+        }
+        
+        //pojedyncza gwiazdka w środku
+        for (int i = 0; i < n / 2; i++)
+        {
+            Space();
+        }
+        StarLn();
+
+        //dolna połówka, symetrycznie do górnej
+        for (int i = n / 2; i > 0; i--)
+        {
+            for (int j = i - 1; j > 0; j--) //spacje przed
+            {
+                Space();
+            }
+            Star();
+            for (int j = 2 - 2 * i; j > 0; j--) //spacje w środku
+            {
+                Space();
+            }
+            StarLn();
+        }
+
+    }
+
+    static void Main(string[] args)
+    {
+        int x = 7;
+        LiteraX(x);
+    }
+}
