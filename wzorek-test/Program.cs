4858cfb57bdcd5c0997f097922d276b961d1566b
Hubert Haładyna <hubert.haladyna@microsoft.wsei.edu.pl>
2023-11-29 20:41:48 +0100

    commit

---
+++ wzorek-test/Program.cs
@@ -0,0 +1,48 @@
+﻿int n = int.Parse(Console.ReadLine());
+Wzorek(n);
+
+static void Wzorek(int n)
+{
+    if (n % 2 == 0)
+    {
+        n--;
+    }
+
+    int middle = n / 2;
+    int spaces = middle;
+
+    spaces = 0;
+
+    for (int i = middle ; i >= 0; i--)
+    {
+        for (int j = 0; j < spaces; j++)
+        {
+            Console.Write(" ");
+        }
+
+        for (int j = 0; j < i * 2 + 1; j++)
+        {
+            Console.Write("*");
+        }
+
+        Console.WriteLine();
+        spaces++;
+    }
+        
+    
+    /*for (int i = 0; i <= middle; i++)  górna połówka nie potrzebna
+    {
+        for (int j = 0; j < spaces; j++)
+        {
+            Console.Write(" ");
+        }
+
+        for (int j = 0; j < i * 2 + 1; j++)
+        {
+            Console.Write("*");
+        }
+
+        Console.WriteLine();
+        spaces--;
+    }*/
+}
